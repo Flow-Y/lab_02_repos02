1b0e45a03a49e8ea21c116e09920791b2f076889
Flow-Y <84350144+Flow-Y@users.noreply.github.com>
2022-10-07 15:35:44 +0300

    first branch_02 commit

--- pr22/pr22.cpp
+++ pr22/pr22.cpp
@@ -5,14 +5,14 @@ using namespace std;
 int main()
 {
 	double x; // вхідний параметр
-	double z1; // результат обчислення 1-го виразу
-	// double z2;  // результат обчислення 2-го виразу
+	//double z1; // результат обчислення 1-го виразу
+	double z2;  // результат обчислення 2-го виразу
 	cout << "x = "; cin >> x;
-	z1 = (cos(x) + sin(x)) / (cos(x) - sin(x));
-	// z2 = (tan(2 * x) + (1 / cos(2 * x)));
+	// z1 = (cos(x) + sin(x)) / (cos(x) - sin(x));
+	z2 = (tan(2 * x) + (1 / cos(2 * x)));
 	cout << '\n';
-	cout << "z1 = " << z1 << '\n';
-	// cout << "z2 = " << z2 << '\n';
+	// cout << "z1 = " << z1 << '\n';
+	cout << "z2 = " << z2 << '\n';
 	cin.get();
 	return 0;
 }
